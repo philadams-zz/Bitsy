1ed58bbc6c62fc2d778973f3883201c625235620
Phil Adams <philadams.net@gmail.com>
2015-02-12 19:43:13 -0500

    add Constants file

---
+++ app/src/main/java/net/philadams/bitsy/Constants.java
@@ -0,0 +1,16 @@
+package net.philadams.bitsy;
+
+/**
+ * Created by phil on 2/12/15.
+ */
+public class Constants {
+
+  public static final int ID_NOTIFY_BINARY_BASIC = 1;
+
+  public static final int ID_BINARY_FALSE = 100;
+  public static final int ID_BINARY_TRUE = 101;
+
+  public static final String KEY_FROM_CONTENT_INTENT = "net.philadams.bitsy.launched_via_notification_default";
+  public static final String KEY_BINARY_RESPONSE = "net.philadams.bitsy.binary_response";
+
+}
