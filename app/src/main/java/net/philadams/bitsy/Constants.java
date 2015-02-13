d6688b3c1fb8edbc7fac7a42b5fe9764a850cc3f
Phil Adams <philadams.net@gmail.com>
2015-02-12 21:02:10 -0500

    add RemoteView-based NRS-11 notification self-report

--- app/src/main/java/net/philadams/bitsy/Constants.java
+++ app/src/main/java/net/philadams/bitsy/Constants.java
@@ -5,12 +5,18 @@ package net.philadams.bitsy;
  */
 public class Constants {
 
+  // notification IDs
   public static final int ID_NOTIFY_BINARY_BASIC = 1;
+  public static final int ID_NOTIFY_NRS = 2;
 
+  // PendingIntent IDs
   public static final int ID_BINARY_FALSE = 100;
   public static final int ID_BINARY_TRUE = 101;
+  public static final int[] ID_NRS = { 200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210 };
 
-  public static final String KEY_FROM_CONTENT_INTENT = "net.philadams.bitsy.launched_via_notification_default";
+  // various keys, commonly for Intent extras
+  public static final String KEY_FROM_CONTENT_INTENT =
+      "net.philadams.bitsy.launched_via_notification_default";
   public static final String KEY_BINARY_RESPONSE = "net.philadams.bitsy.binary_response";
-
+  public static final String KEY_NRS_RESPONSE = "net.philadams.bitsy.nrs_response";
 }
--- app/src/main/java/net/philadams/bitsy/MainActivity.java
+++ app/src/main/java/net/philadams/bitsy/MainActivity.java
@@ -4,11 +4,12 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.RemoteViews;
 import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
@@ -24,30 +25,44 @@ public class MainActivity extends ActionBarActivity {
     if (notificationManager == null) {
       notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
     }
+  }
 
+  @Override
+  public void onNewIntent(Intent intent) {
     // we're only playing with special actions, or specific onClicks in RemoteViews in
     // Bitsy notifications. If someone taps on the notification and prompts the default action,
     // we would normally launch an in-app version of the self-report interface.
     // for now, via getMainActivityPendingIntent, we just display MainActivity with a note
     // explaining this
-    if (getIntent() != null && getIntent().getBooleanExtra(Constants.KEY_FROM_CONTENT_INTENT, false)) {
+    if (intent.getBooleanExtra(Constants.KEY_FROM_CONTENT_INTENT, false)) {
       TextView messageError = (TextView) findViewById(R.id.message_error);
       messageError.setVisibility(View.VISIBLE);
     }
 
     // just dump Bitsy responses into the MainActivity text for now...
-    if (getIntent() != null && getIntent().hasExtra(Constants.KEY_BINARY_RESPONSE)) {
+
+    // binary responses
+    if (intent.hasExtra(Constants.KEY_BINARY_RESPONSE)) {
       String binaryResponse = "No";
-      if (getIntent().getBooleanExtra(Constants.KEY_BINARY_RESPONSE, false)) {
+      if (intent.getBooleanExtra(Constants.KEY_BINARY_RESPONSE, false)) {
         binaryResponse = "Yes";
       }
       TextView messageAlert = (TextView) findViewById(R.id.message_alert);
-      messageAlert.setText(String.format("When asked, \"%s\", you answered, \"%s\".",
+      messageAlert.setText(String.format("When asked \"%s\", you answered \"%s\".",
           getString(R.string.notify_binary_basic_text), binaryResponse));
       messageAlert.setVisibility(View.VISIBLE);
+      notificationManager.cancel(Constants.ID_NOTIFY_BINARY_BASIC);
     }
 
-    notificationManager.cancelAll();
+    // nrs responses
+    if (intent.hasExtra(Constants.KEY_NRS_RESPONSE)) {
+      int nrsResponse = intent.getIntExtra(Constants.KEY_NRS_RESPONSE, -1);
+      TextView messageAlert = (TextView) findViewById(R.id.message_alert);
+      messageAlert.setText(String.format("When asked \"%s\", you answered \"%d/10\".", getString(R.string.notify_nrs_text), nrsResponse));
+      messageAlert.setVisibility(View.VISIBLE);
+      notificationManager.cancel(Constants.ID_NOTIFY_NRS);
+      // TODO:philadams Meter-style update NRS notification RemoteView to highlight selected number
+    }
   }
 
   @Override
@@ -59,6 +74,10 @@ public class MainActivity extends ActionBarActivity {
 
   @Override
   public boolean onOptionsItemSelected(MenuItem item) {
+
+    // let's start by removing all Bitsy notifications - temporary solution
+    notificationManager.cancelAll();
+
     // Handle action bar item clicks here. The action bar will
     // automatically handle clicks on the Home/Up button, so long
     // as you specify a parent activity in AndroidManifest.xml.
@@ -69,6 +88,10 @@ public class MainActivity extends ActionBarActivity {
       notifyBinaryBasic();
       return true;
     }
+    if (id == R.id.notify_nrs11) {
+      notifyNrs11();
+      return true;
+    }
 
     return super.onOptionsItemSelected(item);
   }
@@ -76,11 +99,11 @@ public class MainActivity extends ActionBarActivity {
   public void notifyBinaryBasic() {
 
     // notification base
-    Notification.Builder mBuilder = new Notification.Builder(this)
-        .setContentTitle(getString(R.string.notify_title))
-        .setContentText(getString(R.string.notify_binary_basic_text))
-        .setSmallIcon(R.drawable.ic_launcher)
-        .setContentIntent(getMainActivityPendingIntent());
+    Notification.Builder mBuilder =
+        new Notification.Builder(this).setContentTitle(getString(R.string.notify_title))
+            .setContentText(getString(R.string.notify_binary_basic_text))
+            .setSmallIcon(R.drawable.ic_launcher)
+            .setContentIntent(getMainActivityPendingIntent());
 
     // notification yes/no actions
     Intent noIntent = new Intent(this, MainActivity.class);
@@ -98,12 +121,37 @@ public class MainActivity extends ActionBarActivity {
     mBuilder.addAction(0, getString(R.string.notify_binary_false), noPendingIntent)
         .addAction(0, getString(R.string.notify_binary_true), yesPendingIntent);
 
+    // build and deploy notification
     notificationManager.notify(Constants.ID_NOTIFY_BINARY_BASIC, mBuilder.build());
+  }
+
+  public void notifyNrs11() {
+
+    // attach PendingIntent onClicks to TextViews in RemoteView
+    RemoteViews notificationView = new RemoteViews(getPackageName(), R.layout.notification_nrs11);
+    final int[] reportableValues = {
+        R.id.nrs_0, R.id.nrs_1, R.id.nrs_2, R.id.nrs_3, R.id.nrs_4, R.id.nrs_5, R.id.nrs_6,
+        R.id.nrs_7, R.id.nrs_8, R.id.nrs_9, R.id.nrs_10
+    };
+    for (int i = 0; i < reportableValues.length; i++) {
+      Intent mIntent = new Intent(this, MainActivity.class);
+      mIntent.putExtra(Constants.KEY_NRS_RESPONSE, i);
+      PendingIntent mPendingIntent = PendingIntent.getActivity(this, Constants.ID_NRS[i], mIntent,
+          PendingIntent.FLAG_ONE_SHOT);
+      notificationView.setOnClickPendingIntent(reportableValues[i], mPendingIntent);
+    }
+
+    // build and deploy notification
+    Notification.Builder mBuilder = new Notification.Builder(this)
+        .setSmallIcon(R.drawable.ic_launcher)
+        .setContentIntent(getMainActivityPendingIntent())
+        .setContent(notificationView);
+    notificationManager.notify(Constants.ID_NOTIFY_NRS, mBuilder.build());
   }
 
   private PendingIntent getMainActivityPendingIntent() {
     Intent intent = new Intent(this, MainActivity.class);
-    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+    //intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
     intent.putExtra(Constants.KEY_FROM_CONTENT_INTENT, true);
     PendingIntent pendingIntent =
         PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
